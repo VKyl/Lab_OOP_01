0aefead9504568d48137f6481f9f7431d8d3c467
VKyl <viktor.kyliukh@gmail.com>
2025-03-03 22:00:00 +0200

    declared basics

--- Kyliukh_OOP_01/Triangle.h
+++ Kyliukh_OOP_01/Triangle.h
@@ -13,5 +13,30 @@ public:
 private:
 	Point _a, _b, _c;
 	Segment *_ab_ptr,  *_ac_ptr, *_bc_ptr;
+	// left median impl
+
+public:
+	Triangle(const double x1, const double y1, 
+			 const double x2, const double y2, 
+			 const double x3, const double y3);
+
+	Triangle(const Triangle& t);
+	// Triangle(Triangle&& t); left move constructor decl and iml
+
+
+	Point& a() { return _a; }
+	const Point& a() const { return _a; }
+
+	Point& b() { return _b; }
+	const Point& a() const { return _b; }
+
+	Point& c() { return _c; }
+	const Point& c() const { return _c; }
+
+	Segment& ab();
+	Segment& ac();
+	Segment& bc();
+
+	//const Segment& ab() const; in question
 };
 
